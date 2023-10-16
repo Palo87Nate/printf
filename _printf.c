70d4f8bf0943f43d73f3bd07b7668899432c1ef7
Nathan Nkweto <nathannkwet87@gmail.com>
2023-10-16 06:44:05 -0700

    here is my solution to the first task

--- _printf.c
+++ _printf.c
@@ -1,67 +1,87 @@
+#include <stddef.h>
 #include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * _printf - produces an output in stdout stream
- * @format: format string
+ * printIdentifiers - prints special characters
+ * @next: character after the %
+ * @arg: argument for the indentifier
  * Return: number of characters printed
  */
 
-int _printf(const char *format, ...) {
-    va_list args;
-    int char_count = 0;
+int printIdentifiers(char next, va_list arg)
+{
+	int functsIndex;
+	identifierStruct functs[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"d", print_int},
+		{"i", print_int},
+		{"u", print_unsigned},
+		{"b", print_unsignedToBinary},
+		{"o", print_oct},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"S", print_STR},
+		{NULL, NULL}
+	};
 
-    va_start(args, format);
-
-    while (format && *format) {
-        if (*format != '%') {
-            _putchar(*format);
-            char_count++;
-        } else {
-            format++;
-            if (*format == '\0')
-                break;
-            int printed = 0;
-
-            if (*format == '%') {
-                printed += print_percent();
-            } else {
-                int i;
-                identifierStruct identifierArr[] = {
-                    {"c", print_char},
-                    {"s", print_str},
-                    {"i", print_int},
-                    {"d", print_int},
-                    {"u", print_unsigned},
-                    {"b", print_unsignedToBinary},
-                    {"o", print_oct},
-                    {"x", print_hex},
-                    {"X", print_HEX},
-                    {"S", print_STR},
-                    {NULL, NULL}
-                };
-
-                for (i = 0; identifierArr[i].indentifier; i++) {
-                    if (*format == identifierArr[i].indentifier[0]) {
-                        printed += identifierArr[i].printer(args);
-                        break;
-                    }
-                }
-
-                if (identifierArr[i].indentifier == NULL)
-                {
-                    _putchar('%');
-                    _putchar(*format);
-                    printed = 2;
-                }
-            }
-
-            char_count += printed;
-        }
-
-        format++;
-    }
-
-    va_end(args);
-    return char_count;
+	for (functsIndex = 0; functs[functsIndex].indentifier != NULL; functsIndex++)
+	{
+		if (functs[functsIndex].indentifier[0] == next)
+			return (functs[functsIndex].printer(arg));
+	}
+	return (0);
 }
 
+/**
+ * _printf - act like printf
+ * @format: character string composed of zero or more directives
+ * Return:  number of characters printed or -1 for incomplete identifier error
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i;
+	int identifierPrinted = 0, charPrinted = 0;
+	va_list arg;
+
+	va_start(arg, format);
+	if (format == NULL)
+		return (-1);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			charPrinted++;
+			continue;
+		}
+		if (format[i + 1] == '%')
+		{
+			_putchar('%');
+			charPrinted++;
+			i++;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+			return (-1);
+
+		identifierPrinted = printIdentifiers(format[i + 1], arg);
+		if (identifierPrinted == -1 || identifierPrinted != 0)
+			i++;
+		if (identifierPrinted > 0)
+			charPrinted += identifierPrinted;
+
+		if (identifierPrinted == 0)
+		{
+			_putchar('%');
+			charPrinted++;
+		}
+	}
+	va_end(arg);
+	return (charPrinted);
+}
